6f30328313c043ef3f2be775a041c37da7896e37
Junior Wender <juniorwender1996@gmail.com>
2023-05-16 16:00:00 -0300

    projeto microfront

---
+++ ReactFront/src/root.component.jsx
@@ -0,0 +1,8 @@
+export default function Root(props) {
+  return (
+    <div>
+      <h1>React Front</h1>
+      <div> Esse componente foi criado e renderizado pela aplicação em react </div>
+    </div>
+  );
+}
